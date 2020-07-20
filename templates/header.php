da30caf6cff3558c83221677294eef79bd924707
Aleksandr Derzhavsky <antarctida.inc@gmail.com>
2020-07-20 22:10:41 +0300

    Add basic markup header, navbar.

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,44 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Good Blog</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <h1 class="text-center">Name of my Blog</h1>
+    <div class="row">
+        <div class="col-sm-9">
+            <a href="" class="nav-link text-right">Sing In</a>
+        </div>
+        <div class="col-sm-3">
+      <form class="form-inline my-2 my-lg-0"">
+        <input type="text" class="form-control mr-sm-2" placeholder="Search.." aria-label="Search"><!-- search by id -->
+          <button class="btn btn-dark">Search</button>
+       </form>
+        </div>
+    </div>
+            </div>
+</div>
+<hr>
+    <nav class="navbar navbar-expand-lg navbag-light bg-light">
+        <a href="./" class="nav-link">Home</a>
+        <a href="?pages=blog" class="nav-link">Blog</a>
+        <a href="?pages=about" class="nav-link">About us</a>
+<!--        <a href="" class="nav-link">Sing in</a>-->
+    </nav>
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
