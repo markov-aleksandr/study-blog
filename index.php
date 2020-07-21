38c9294b539386df71c28e299ee9267550c247e7
Aleksandr Derzhavsky <antarctida.inc@gmail.com>
2020-07-21 10:20:29 +0300

    Add banal routing

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
+<?
 require('./templates/header.php');
+
+if (isset($_GET['pages'])){  
+    $page = htmlentities($_GET['pages']);
+    $path = './pages/'.$page.'.php';
+    if (file_exists($path)){
+        include($path);
+    }
+}else{
+    include('./pages/home.php');
+}
+
+require('./templates/footer.php');
